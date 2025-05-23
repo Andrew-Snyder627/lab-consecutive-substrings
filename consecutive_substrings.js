a093fc83eaff0ecde6f48886bfcdf78929756ffa
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-05-23 11:04:09 -0400

    Saving work for submission

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,36 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  const result = [];
+  // Outer loop for each start position of the string
+  for (let start = 0; start < string.length; start++) {
+    // Inner loop for each possible end position after the start
+    // We got up to string.length because we need to capture the substrings that end at the last character
+    for (end = start + 1; end <= string.length; end++) {
+      // Add substring from start to end
+      result.push(string.slice(start, end));
+    }
+  }
+  return result;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings("abc"));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings("a"));
+
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(""));
+
+  // Longer string to test
+  console.log(
+    "Expecting: ['w', 'wa', 'wat', 'wate', 'water', 'a', 'at', 'ate', 'ater', 't', 'te', 'ter', 'e', 'er', 'r']"
+  );
+  console.log("=>", consecutiveSubstrings("water"));
 }
 
 module.exports = consecutiveSubstrings;
-
-
